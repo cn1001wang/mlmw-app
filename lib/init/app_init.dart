baf880dde76492fcc8fbbd33b5d9ec4f734bc2c1
cn1001wang <34416681+cn1001wang@users.noreply.github.com>
2021-03-15 12:48:46 +0800

    fix runZoned

--- lib/init/app_init.dart
+++ lib/init/app_init.dart
@@ -16,20 +16,21 @@ class AppInit {
     FlutterError.onError = (FlutterErrorDetails details) {
       reportErrorAndLog(details);
     };
-    runZoned<Future<Null>>(
+    runZonedGuarded(
       () async {
         callback();
       },
+      //未捕获的异常的回调
+      (Object error, StackTrace stack) async {
+        var details = makeDetails(error, stack);
+        reportErrorAndLog(details);
+      },
+      // 收集日志
       zoneSpecification: ZoneSpecification(
         print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
           collectLog(parent, zone, line); // 收集日志
         },
       ),
-      //未捕获的异常的回调
-      onError: (Object obj, StackTrace stack) {
-        var details = makeDetails(obj, stack);
-        reportErrorAndLog(details);
-      },
     );
   }
 
@@ -45,6 +46,6 @@ class AppInit {
 
   // 构建错误信息
   static FlutterErrorDetails makeDetails(Object obj, StackTrace stack) {
-    return FlutterErrorDetails(stack: stack);
+    return FlutterErrorDetails(stack: stack, exception: obj);
   }
 }
